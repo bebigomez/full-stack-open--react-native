c7f38e4ee2ed8e444995195c412f0833254c8b16
Victor Gomez <vhgomez.py@gmail.com>
2024-04-28 18:59:53 -0400

    add theme.js and Text component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,6 @@
-import RepositoryList from "./RepositoryList";
+import RepositoryList from "./RepositoryList"
 
-import { View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import { Constants, View, StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
   container: {
@@ -9,15 +8,14 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     flexShrink: 1,
   },
-});
+})
 
 const Main = () => {
   return (
     <View style={styles.container}>
       <RepositoryList />
     </View>
-  
-)
+  )
 }
 
-export default Main;
+export default Main
