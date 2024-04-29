920ad7d6cdcbbcdea203c68c5414deb4c8c6d3bf
Victor Gomez <vhgomez.py@gmail.com>
2024-04-29 17:07:55 -0400

    10.9 done

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,5 +1,6 @@
-import { Text, TextInput, Pressable, View, StyleSheet } from 'react-native';
-import { useFormik } from 'formik';
+import { Text, TextInput, Pressable, View, StyleSheet } from 'react-native'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 import theme from '../theme'
 
 const styles = StyleSheet.create({
@@ -7,63 +8,78 @@ const styles = StyleSheet.create({
     display: 'flex',
     padding: 20,
     backgroundColor: 'white',
-    gap: 10
+    gap: 10,
   },
   input: {
     padding: 8,
     borderWidth: 1,
     borderColor: theme.colors.lightGray,
-    borderRadius: 5
+    borderRadius: 5,
+  },
+  errorBorder: {
+    borderColor: theme.colors.error,
   },
   submitButton: {
     backgroundColor: theme.colors.primary,
     paddingVertical: 8,
     borderRadius: 5,
     alignItems: 'center',
-  }
+  },
+})
+
+const validationSchema = yup.object().shape({
+  username: yup.string().required('username is required'),
+  password: yup.string().required('password is required'),
 })
 
 const initialValues = {
   username: '',
   password: '',
-};
+}
 
 const SignInForm = ({ onSubmit }) => {
   const formik = useFormik({
     initialValues,
+    validationSchema,
     onSubmit,
-  });
+  })
 
   return (
     <View style={styles.container}>
       <TextInput
-        style={styles.input}
-        placeholder="username"
+        style={[styles.input, formik.touched.username && formik.errors.username ? styles.errorBorder : null]}
+        placeholder='username'
         value={formik.values.username}
         autoCapitalize='none'
         onChangeText={formik.handleChange('username')}
       />
+      {formik.touched.username && formik.errors.username && (
+        <Text style={{ color: theme.colors.error }}>{formik.errors.username}</Text>
+      )}
       <TextInput
-        style={styles.input}
-        placeholder="password"
+        style={[styles.input, formik.touched.password && formik.errors.password ? styles.errorBorder : null]}
+        placeholder='password'
         value={formik.values.password}
         autoCapitalize='none'
         secureTextEntry
         onChangeText={formik.handleChange('password')}
       />
-      <Pressable  style={styles.submitButton} onPress={formik.handleSubmit}>
+      {formik.touched.password && formik.errors.password && (
+        <Text style={{ color: theme.colors.error }}>{formik.errors.password}</Text>
+      )}
+      <Pressable style={styles.submitButton} onPress={formik.handleSubmit}>
         <Text style={{ color: 'white' }}>Sign in</Text>
       </Pressable>
     </View>
-  );
-};
+  )
+}
 
 const SignIn = () => {
   const onSubmit = (values) => {
-    console.log(values);
-  };
+    console.log(values)
+  }
 
-  return <SignInForm onSubmit={onSubmit} />;
-};
+  return <SignInForm onSubmit={onSubmit} />
+}
 
-export default SignIn;
+export default SignIn
--- src/theme.js
+++ src/theme.js
@@ -1,22 +1,23 @@
 const theme = {
   colors: {
-    textPrimary: "#24292e",
-    textSecondary: "#586069",
-    primary: "#0366d6",
-    appBarBG: "#24292e",
-    mainBackground: "#e1e4e8",
-    lightGray: "#D3D3D3"
+    textPrimary: '#24292e',
+    textSecondary: '#586069',
+    primary: '#0366d6',
+    appBarBG: '#24292e',
+    mainBackground: '#e1e4e8',
+    lightGray: '#D3D3D3',
+    error: '#d73a4a',
   },
   fontSizes: {
     body: 14,
     subheading: 16,
   },
   fonts: {
-    main: "System",
+    main: 'System',
   },
   fontWeights: {
-    normal: "400",
-    bold: "700",
+    normal: '400',
+    bold: '700',
   },
 }
 
